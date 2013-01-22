2cc19033e5d8008ce96efcc2c3782387215d0a75
Marco Zavatta <zavaboss@gmail.com>
2013-01-22 11:44:00 +0100

    much better definition of Streaming Manager interface

--- jni/ze_coap_streaming_manager.h
+++ jni/ze_coap_streaming_manager.h
@@ -1,77 +1,156 @@
-
-// XXX: INITIAL SUPPORT ONLY FOR ONE OBSERVER FOR EACH SENSOR
-ze_sensor_t ze_streaming_state[13];
-
-// With the resource uri and the destination address we can associate each sensor
-// to a specific resource in the CoAP realm
-// TODO: the reliability policy might be parametrized too
-int add_stream(int sensor, coap_address_t *dest, int freq, int deadline);
-void stop_stream(int sensor, 	coap_address_t *dest); //if destination NULL, return global sensor info
-int is_streaming(int sensor, 	coap_address_t *dest); //if destination NULL, return global sensor info
-
-
+/*
+ * ZeSense Streaming Manager
+ * -- core streaming module
+ *
+ * Marco Zavatta
+ * <marco.zavatta@telecom-bretagne.eu>
+ * <marco.zavatta@mail.polimi.it>
+ */
 
 /**
- * Returns a single sample of @p sensor,
- * in the container @p data
- * Returns the length of the @p data
+ * Binds a sensor source @p sensor to a specific @p URI.
+ * Only one URI can be associated to a sensor source;
+ * If an association is already in place, it will be overwritten.
  *
- * The reliability policy here doesn't concern us since the actual send is done
- * by somebody else, which will decide the policy on his own
+ * @param mngr		The Streaming Manager context
+ * @param sensor	One of the available sensor sources
+ * @param uri		String variable to associate to @p sensor
+ *
+ * @return Zero on success and first association,
+ * @c SM_URI_REPLACED if successful and association overwritten
+ * @c SM_ERROR on failure
  */
-int get_single_sample(int sensor, unsigned char *data);
+int bind_source(stream_context_t *mngr, int sensor, str uri);
+
+/*
+ * Binds the given @p server to Streaming Manager @p mngr
+ * in order to relay notifications to him.
+ */
+int bind_server(stream_context_t *mngr, coap_context_t *server);
+
+/**
+ * Starts a stream of notifications of samples
+ * collected by @p sensor to destination @p dest.
+ * The client will be notified at the given frequency @p freq.
+ * Only one specific destination is allowed for a sensor source.
+ * If already existing, it will be replaced.
+ * The requested @p freq will be rounded to multiples of 10Hz
+ * and capped to the maximum running frequency
+ * of the sensor source.
+ *
+ * TODO: many other parameters may be added in the future
+ * for example reliability policy, the deadlines or
+ * the timestamps policy (for now they are fixed and hard-coded)
+ * maybe even in the form of some query language
+ *
+ * @param mngr		The Streaming Manager context
+ * @param sensor	The sensor source of data
+ * @param dest		The IP/port coordinates of the destination
+ * @param freq		The frequency of notifications
+ *
+ * @return Zero on success, @c SM_STREAM_REPLACED if the new stream
+ * replaced an existing one, @c SM_ERROR on failure
+ */
+int start_stream(stream_context_t *mngr, int sensor, coap_address_t *dest, int freq);
+
+/**
+ * Stops the stream of notifications from @p sensor
+ * to destination @p dest.
+ *
+ * @param mngr		The Streaming Manager context
+ * @param sensor	The sensor source of data
+ * @param dest		The IP/port coordinates of the destination
+ *
+ * @return Zero on success, @c SM_ERROR on failure
+ * (e.g. the stream does not exist)
+ */
+int stop_stream(stream_context_t *mngr, int sensor, coap_address_t *dest);
+
+/**
+ * Checks if a stream from @p sensor to destination @p dest
+ * is currently running.
+ *
+ * @param mngr		The Streaming Manager context
+ * @param sensor	The sensor source of data
+ * @param dest		The IP/port coordinates of the destination
+ *
+ * @return Zero if positive answer, @c SM_NEGATIVE otherwise
+ */
+int is_streaming(stream_context_t *mngr, int sensor, 	coap_address_t *dest);
+
+/**
+ * Returns a single sample of @p sensor
+ * Is is put in the container @p data of length @p length
+ * The memory allocated for @p data will not be freed.
+ *
+ * @param mngr		The Streaming Manager context
+ * @param sensor	The sensor source of data
+ * @param data		Data bytes
+ * @param length	Length of the @p data field
+ *
+ * @return Zero on success, @c SM_ERROR on failure
+ */
+int get_single_sample(stream_context_t *mngr, int sensor,
+		unsigned char *data, int length);
+
+
+typedef struct stream_context_t {
+	/* Sensors sources available for streaming */
+	ze_sensor_t sensor[13];
+
+	/* The server we're sending the streams to */
+	coap_context_t server = NULL;
+};
+
 
 typedef struct ze_sensor_t {
-
-	// Association sensor-resource
+	/* Association sensor-resource */
 	int sensor;
 	str uri;
-	/* this copy is very dangerous
-	 * since we will use it to send messages
-	 * we must remember to NULL this pointer when a resource gets deleted
-	 * and to check before making a notification that this pointer is not NULL
-	coap_resource_t *resource;
-	*/
 
-	/*
-	 * Utility to be quick when answering a one-shot sensor value request
-	 */
-	ASensorEvent last_event;
+	/* Quick access to last known sensor value */
+	ASensorEvent last_known_event;
 
+	/* List of streams registered on this sensor */
 	//ze_single_stream_t *streams = NULL;
 	ze_single_stream_t stream = NULL;
 
-	// Streaming manager local status variables
+	/* Local status variables */
 	int freq;
 	int last_wts;
 	int last_rtpts;
 };
 
-typedef struct ze_single_stream_t {
+typedef struct ze_stream_t {
+	ze_single_stream_t *next = NULL;
 
-	ze_single_stream_t *next;
-
-	/* In some way this is the key,
+	/* In some way this is the lookup key,
 	 * no two elements with the same dest will be present in the list
-	 * ss mandated by draft-coap-observe
+	 * as mandated by draft-coap-observe-7
 	 */
 	coap_address_t dest;
 
-	// User specified
+	/* Client specified */
 	int freq;
-	int deadline;
 
-	// Streaming manager local status variables
+	/* Streaming Manager local status variables */
 	int last_wts;	//Last wallclock timestamp
 	int last_rtpts;	//Last RTP timestamp
-	int freq_div;	//Frequency divider (automatically assigned)
+	int freq_div;	//Frequency divider
 };
 
-typedef struct ze_sensor_coap_payload {
+typedef struct ze_payload_container_t {
+	ze_payload_t pyl;
+	int length;
+};
+
+typedef struct ze_payload_t {
 	int64_t wts;
 	int rtpts;
-	int debugpayload;
+	unsigned char *pyl;
 };
+
+
 
 
 
